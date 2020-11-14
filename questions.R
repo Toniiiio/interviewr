90fef4baee00dd3ee5537cbae6706c283cb606f0
Toniiiio <User11@digitallab.cloud>
2020-11-14 10:15:08 +0100

    adding hints

--- questions.R
+++ questions.R
@@ -1,9 +1,10 @@
 options(stringsAsFactors = FALSE)
 questions <- list()
 
-questions[["Multiples of 3 and 5"]] <- data.frame(
+questions[["Multiples of 3 and 5"]] <- list(
   text = "If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.",
   time = 300,
+  hints = c("The modulus can be calculated in R with '%%'", "7/3 is 2 remaining one. 7%%3 is 1.", "Type in R: '6%%3'", "Type in R: '6%%3' & '6%%5'"),
   solution = 233168,
   answer = "sum = 0
             for(nr in 1:999){
@@ -12,20 +13,19 @@ questions[["Multiples of 3 and 5"]] <- data.frame(
             print(sum)"
 )
 
-questions[["FizzBuzz"]] <- data.frame(
+questions[["FizzBuzz"]] <- list(
   text = 'Classical FizzBuzz: Print every number between 1 and 100, However for numbers that are multiples of 3 print "Fizz" and for numbers that are multiples of five print "Buzz."   For numbers that are multiples of both 3 and 5 print "FizzBuzz".',
   time = 300
 )
 
-questions[["Find prime numbers"]] <- data.frame(
+questions[["Find prime numbers"]] <- list(
   text = "Sum of all prime numbers below 5000 that can be devided by 72",
   time = 120,
   solution = 0,
   answer = 0
 )
 
-questions[["Compute standard deviation from scratch"]] <- data.frame(
-  text = "Generate 100 random numbers with seed 50 and write a standard deviation fct.",
+questions[["Compute standard deviation from scratch"]] <- list(
+  text = "Generate 100 random numbers with seed 50 and write a standard deviation function.",
   time = 600
 )
-
--- run_test.R
+++ run_test.R
@@ -1,5 +1,14 @@
-pckgs <- c("shiny", "shinyAce", "shinyjs", "glue", "V8", "reticulate", "Rcpp")
+user_info <- data.frame(
+  id = "awd2z",
+  name = "",
+  participated = FALSE
+)
+#write.csv2(x = user_info, file = "users.txt")
+#
 
+
+pckgs <- c("shiny", "shinyAce", "shinyjs", "glue", "V8", "reticulate", "Rcpp", "magrittr")
+#VG9uaW8gTGllYnJhbmQ=
 
 mssng_pckg <- pckgs[!(pckgs %in% installed.packages())]
 if(length(mssng_pckg)) install.packages(mssng_pckg)
@@ -13,10 +22,12 @@ sapply(pckgs, library, character.only = TRUE)
 # library(reticulate)
 # library(Rcpp)
 
-setwd("C:/Users/User11/Desktop/Transfer/TMP/AWS_Backup/OnlineCodeTest")
+#setwd("C:/Users/User11/Desktop/Transfer/TMP/AWS_Backup/OnlineCodeTest")
 options(stringsAsFactors = FALSE)
 source("questions.R")
 
+
+  
 language_choices <- c("R", "Python", "SQL", "Javascript", "C++")
 
 selectedQuestionIndex <- c(1, 3, 4)
@@ -32,9 +43,9 @@ py_evaluate <- function(code) {
   globals <- reticulate::py_eval("globals()", convert = FALSE)
   locals <- globals
   parsed <- builtins$compile(code, "<string>", "single")
-  a <<- builtins$eval(parsed, globals, locals)
+  builtins$eval(parsed, globals, locals)
 }
-x <- capture.output(py_evaluate("22"))
+py_evaluate("22")
 
 
 
@@ -44,17 +55,120 @@ themes <- getAceThemes()
 ui <- shinyUI(
   fluidPage(
     useShinyjs(),
+    # uiOutput("ui2"),
     h2("Online Code Test:"),
-    # selectInput("mode", "Mode: ", choices = modes, selected = "r"),
-    # selectInput("theme", "Theme: ", choices = themes, selected = "ambience"),
-    uiOutput("config"),
-    uiOutput("codeEditor"),
-    uiOutput("nextQue")
+    uiOutput("test_ui"),
   )
 )
 
 server <- shinyServer(function(input, output, session) {
-  
+
+
+
+  observe({
+    global$user_info <- read.csv2(file = "users.txt")
+  })
+
+  observe({
+
+    global$user_id <- urltools::param_get(session$clientData$url_search)$id
+    req(global$user_id)
+    global$is_valid_id <- global$user_id %in% global$user_info$id
+
+    if(is.null(global$user_id)) global$is_valid_id <- FALSE
+
+    if(global$is_valid_id){
+      global$user_name <- global$user_info[global$user_info$id == global$user_id]$name
+      # global$user_name = print(RCurl::base64Decode(x))
+
+      global$has_participated = global$user_info[global$user_info$id == global$user_id]$participated
+      if(global$has_participated) print("The test was already taken for the provided id.")
+
+    }
+
+  })
+
+  global <- reactiveValues(
+    # time_left  = time_to_use,
+    test_started = FALSE,
+    question_nr = 1,
+    questions = questions,
+    is_valid_id = NULL,
+    has_participated = FALSE,
+    start_time = Sys.time() + 1e6, # avoid to trigger interruption message before test was started
+    time_to_use = time_to_use,
+    display = TRUE,
+    hint_nr = 0,
+    answersCode = list(),
+    answersResult = list(),
+    answersCorrect = list(),
+    finished = FALSE
+  )
+
+  observeEvent(input$start_test, {
+    global$test_started <- TRUE
+  })
+
+  observeEvent(global$set_as_participated, {
+    global$start_time <- Sys.time()
+    global$user_info[global$user_info$id == global$user_id]$participated <- TRUE
+    #write.csv2(x = global$user_info, file = "users.txt")
+  })
+
+  output$test_ui <- renderUI({
+
+    req(!is.null(global$is_valid_id))
+
+    if(global$test_started){
+
+      out <- tagList(
+        uiOutput("config"),
+        uiOutput("codeEditor"),
+        uiOutput("nextQue")
+      )
+
+      global$set_as_participated <- TRUE
+
+    }else if(!global$is_valid_id){
+
+      out <- h5(paste0("We cant find an open test for the given id!"))
+
+    }else if(global$has_participated){
+
+      out <- h5(paste0("The test was already taken for the provided id!"))
+
+    }else{
+
+      out <- tagList(
+        h5(paste0("Welcome ", global$user_name, "!")),
+        actionButton(inputId = "start_test", label = "Start test!")
+      )
+
+    }
+
+    return(out)
+
+  })
+
+
+  observe({
+
+    hint_title <- "Hint"
+    req(input$show_hint)
+
+    if(input$show_hint){
+      isolate(global$hint_nr <- global$hint_nr + 1)
+
+      showModal(modalDialog(
+        title = hint_title,
+        global$questions[[global$question_nr]]$hints[global$hint_nr]
+#        "What do all points on the circle have in common?."
+      ))
+
+    }
+  })
+
+
   observe({
     req(global$time_left)
     if(global$time_left == 30){
@@ -65,49 +179,59 @@ server <- shinyServer(function(input, output, session) {
       ))
     }
   })
-  
+
   output$nextQue <- renderUI({
     if(!global$finished){
-      actionButton("nextQuestion", "Submit / Next Question")      
+      actionButton("next_question", "Submit / Next Question")
     }
   })
-  
+
   output$config <- renderUI({
+
+    global$amt_hints <- length(global$questions[[global$question_nr]]$hints)
+    
+    # could use shinyjs::disable that does not work when being integrated
+    if(global$amt_hints <= global$hint_nr){
+      hint <- NULL 
+    }else{
+      hint <- actionButton(inputId = "show_hint", label = paste0("Show hint (", global$hint_nr, " / ", global$amt_hints,")"))
+    }
+
     if(!global$finished){
-      tagList(
+      out <- tagList(
         htmlOutput("time_left"),
         selectInput(inputId = "language", label = "Select Language: ", choices = language_choices),
+        hint,
         textOutput("question")
       )
+      return(out)
     }
   })
+
   
+  ## not working  
+  # observeEvent((input$dss | global$hint_nr), {
+  #   req(global$amt_hints)
+  #   if(global$amt_hints > global$hint_nr){
+  #     print("yoo")
+  #     shinyjs::disable("show_hint")
+  #   }
+  # }, priority = -1)
+
   # not working
   observe({
     req(input$comment)
     print(input$comment)
-    shinyjs::runjs("$('#comment').attr('maxlength', 10)")    
+    shinyjs::runjs("$('#comment').attr('maxlength', 10)")
   })
-  
-  global <- reactiveValues(
-    # time_left  = time_to_use,
-    questionNr = 1, 
-    start_time = Sys.time(),
-    time_to_use = time_to_use,
-    display = TRUE,
-    answersCode = list(),
-    answersResult = list(),    
-    answersCorrect = list(),
-    finished = FALSE
-  )
-  
+
   observe({
     invalidateLater(millis = 1000)
     global$time_left <- round(difftime(global$start_time, Sys.time(), units = "secs") + global$time_to_use)
   })
-  
+
   output$time_left <- renderText({
-    print(global$time_left)
+
     if(global$time_left < 1){
       return("Sorry, you dont have time left.")
     }else if(global$time_left > 0 & global$time_left < 30){
@@ -115,14 +239,17 @@ server <- shinyServer(function(input, output, session) {
     }
     glue("Seconds left: {global$time_left}")
   })
-  
+
   observeEvent(global$time_left, {
     global$display <- global$time_left > 0
   })
-  
+
   output$codeEditor <- renderUI({
-    input$nextQuestion
+    
+    input$next_question
+    
     if(global$display & !global$finished){
+      
       isolate({
         fluidRow(
           column(
@@ -139,49 +266,59 @@ server <- shinyServer(function(input, output, session) {
               verbatimTextOutput("output")
             )
           )
-        )      
-      })      
+        )
+      })
     }else if(!global$display & !global$finished){
       textInput(
-        inputId = "comment", 
-        label = "Optional: Leave a comment - (What would you have done with more time)", 
+        inputId = "comment",
+        label = "Optional: Leave a comment - (What would you have done with more time)",
         placeholder = "I would have vectorised the code."
       )
     }else{
       h4("You are finished! Thank you for participating.")
     }
-    
+
   })
-  
+
   output$question <- renderText({
-    questions[[global$questionNr]]$text
+    global$questions[[global$question_nr]]$text
   })
-  
-  observeEvent(eventExpr = input$nextQuestion, {
-    global$answersCode[[global$questionNr]] <- input$code
-    correctSolution <- questions[[global$questionNr]]$solution
+
+  observeEvent(eventExpr = input$next_question, {
+    
+    global$answersCode[[global$question_nr]] <- input$code
+    correctSolution <- global$questions[[global$question_nr]]$solution
     providedSolution <- eval(parse(text = isolate(input$code)))
-    global$answersResult[[global$questionNr]] = providedSolution
-    global$answersCorrect[[global$questionNr]] = correctSolution == providedSolution
-    print(global$answersCode)
+    global$answersResult[[global$question_nr]] = providedSolution
+    global$answersCorrect[[global$question_nr]] = correctSolution == providedSolution
+    # print(global$answersCode)
     print(global$answersResult)
-    print(global$answersCorrect)    
-    
-    
-    if(global$questionNr < length(questions)){
-      global$questionNr <- global$questionNr + 1
-      global$time_to_use <- questions[[global$questionNr]]$time
+    # print(global$answersCorrect)
+
+    if(global$question_nr < length(global$questions)){
+      
+      global$question_nr <- global$question_nr + 1
+      global$time_to_use <- global$questions[[global$question_nr]]$time
       global$start_time <- Sys.time()
-      print(global$answers)
+
     }else{
+
+
       global$finished <- TRUE
+      save_global <- reactiveValuesToList(global)
+      save(x = save_global, file = paste("results_", global$user_info$id, ".RData"))
+      rmarkdown::render("test.Rmd", params = list(
+        test_data = save_global
+      ))
+      print("rr4")
+      
     }
   })
-  
+
   output$uiAce <- renderUI({
-    aceEditor("code", mode = "r", height = "200px", value = "1") #init[[input$language]]   
+    aceEditor("code", mode = "r", height = "200px", value = "1") #init[[input$language]]
   })
-  
+
   observe({
     updateAceEditor(
       session,
@@ -193,9 +330,11 @@ server <- shinyServer(function(input, output, session) {
       showInvisibles = TRUE
     )
   })
-  
+
   output$output <- renderPrint({
+    
     input$eval
+    
     if(input$language == "R"){
       return(eval(
         parse(
@@ -203,18 +342,19 @@ server <- shinyServer(function(input, output, session) {
         )
       ))
     }
+    
     if(input$language == "Javascript"){
       ctx <- v8();
       return(ctx$eval(isolate(input$code)))
     }
-    
+
     if(input$language == "Python"){
-      print(input$code)
-      print(py_evaluate(input$code))
-      print(88)
+      # print(input$code)
+      # print(py_evaluate(input$code))
+      # print(88)
       isolate({
         print(py_evaluate(input$code))
-        # return(py_evaluate(input$code))        
+        # return(py_evaluate(input$code))
       })
     }
   })
